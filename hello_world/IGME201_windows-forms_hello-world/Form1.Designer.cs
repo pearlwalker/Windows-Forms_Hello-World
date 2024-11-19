abfee9a64cf3a0ae137302667fec990756e95764
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-19 10:08:38 -0500

    Add Visual Studio repo
    Added Visual Studio repo files to GitHub

---
+++ hello_world/IGME201_windows-forms_hello-world/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace IGME201_windows_forms_hello_world
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bnClickThis
+            // 
+            this.bnClickThis.Location = new System.Drawing.Point(329, 189);
+            this.bnClickThis.Name = "bnClickThis";
+            this.bnClickThis.Size = new System.Drawing.Size(111, 23);
+            this.bnClickThis.TabIndex = 0;
+            this.bnClickThis.Text = "Click this :3";
+            this.bnClickThis.UseVisualStyleBackColor = true;
+            this.bnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(326, 215);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(44, 16);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.bnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
---
+++ hello_world/IGME201_windows-forms_hello-world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IGME201_windows_forms_hello_world
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
